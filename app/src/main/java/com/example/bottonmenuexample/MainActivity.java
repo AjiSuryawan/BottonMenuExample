c2fe5f04a08034bb06676632a82a0e788242335c
Aji Suryawan <kazumovie132@gmail.com>
2024-03-08 10:03:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bottonmenuexample/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.bottonmenuexample;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
+import com.example.bottonmenuexample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnItemSelectedListener(navListener);
+
+        // Load the default fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new Fragment1())
+                .commit();
+    }
+
+    private BottomNavigationView.OnItemSelectedListener navListener =
+            new BottomNavigationView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    if (item.getItemId() == R.id.menu_item1) {
+                        selectedFragment = new Fragment1();
+                    } else if (item.getItemId() == R.id.menu_item2) {
+                        selectedFragment = new Fragment2();
+                    } else if (item.getItemId() == R.id.menu_item3) {
+                        selectedFragment = new Fragment3();
+                    }
+
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container, selectedFragment)
+                            .commit();
+
+                    return true;
+                }
+            };
+}
